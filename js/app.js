d5199678e026faa4e134fac3dd2c4c08f59b5747
kyawsan84yay <kyawsan84yay@gmail.com>
2022-08-29 00:48:51 +0630

    29-8-2022 12:48.A.M.

---
+++ js/app.js
@@ -0,0 +1,26 @@
+// console.log('hay');
+
+$(document).ready(function(){
+
+    // console.log('hi');
+
+    // Start Typed Js
+
+        var typed = new Typed("#banner-h2",{
+            strings :[
+                "Kyaw San.",
+                "Professional Web Developer.",
+                "Professional Software Engineer.",
+                "CEO at CodeFlash."
+            ],
+            typeSpeed: 100,
+            backSpeed: 25,
+            loop: true,
+        });
+
+    // End Typed Js
+
+
+
+
+});
