dac6657bf273a18477025fed95b01340b71a9c76
kyawsan84yay <kyawsan84yay@gmail.com>
2022-08-30 20:58:20 +0630

    30/8/2022 8:58.P.M.

--- js/app.js
+++ js/app.js
@@ -13,12 +13,27 @@ $(document).ready(function(){
                 "Professional Software Engineer.",
                 "CEO at CodeFlash."
             ],
-            typeSpeed: 100,
-            backSpeed: 25,
+            typeSpeed: 150,
+            backSpeed: 45,
             loop: true,
         });
 
     // End Typed Js
+    
+    // Start ScrollTop
+
+    $(window).scroll(function(){
+        let getScrollTop = $(this).scrollTop();
+        // console.log(getScrollTop);
+
+        if(getScrollTop >= 200){
+            $(".navbar").removeClass("hidenavs");
+        }else{
+            $(".navbar").addClass("hidenavs navs");
+        }
+    });
+
+    // End ScrollTop
 
 
 
